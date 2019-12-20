316ebfa0ee44453f77659bd9108d1010649e4635
dj-hirrot <hirotoshibutani@gmail.com>
2019-12-20 13:47:45 +0900

    update: add sinario

--- test/models/blog_test.rb
+++ test/models/blog_test.rb
@@ -11,4 +11,24 @@ class BlogTest < ActiveSupport::TestCase
   test 'should valid' do
     assert @blog.valid?
   end
+
+  test 'title should be present' do
+    @blog.title = ''
+    assert_not @blog.valid?
+  end
+
+  test 'body should be present' do
+    @blog.body = ''
+    assert_not @blog.valid?
+  end
+
+  test 'title should not be too short' do
+    @blog.title = 'a'
+    assert_not @blog.valid?
+  end
+
+  test 'body should not be too short' do
+    @blog.body = 'a'
+    assert_not @blog.valid?
+  end
 end
